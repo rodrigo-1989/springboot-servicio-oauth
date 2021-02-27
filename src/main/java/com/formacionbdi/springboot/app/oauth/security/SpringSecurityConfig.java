6640af236a742eb0e6dddd5380bf5589fe282971
rodrigo <rodrigo_l321@hotmail.com>
2021-02-27 08:53:47 -0500

    Notificacion ExitoError

--- src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
+++ src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.formacionbdi.springboot.app.oauth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationEventPublisher authenticationEventPublisher;
 
     @Autowired
     private UserDetailsService usuarioService;
@@ -23,7 +27,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth.userDetailsService(this.usuarioService).passwordEncoder(passworEncoder());
+       auth.userDetailsService(this.usuarioService).passwordEncoder(passworEncoder())
+       .and().authenticationEventPublisher(authenticationEventPublisher);
     }
 
     @Override
